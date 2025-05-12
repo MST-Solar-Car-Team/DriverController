0b1b9002abf85fe132dabe28098dfe639940bc0a
Rhys Winslett <rew86w@mst.edu>
2025-05-11 22:17:51 -0500

    progress

--- src/telemetry/serial_log.cpp
+++ src/telemetry/serial_log.cpp
@@ -19,28 +19,15 @@ void PedalPacket::send_bytes() {
   data[3] = uint8_t((pedal >> 8) & 0xFF);
   data[4] = uint8_t(pedal & 0xFF);
 
-  // probably won't need these because of line 12
-  // rhys you forgot data[8] = 0x00;
-  // i'm not fixing it because i want to embarrass you
-  data[5] = 0x00;
-  data[6] = 0x00;
-  data[7] = 0x00;
-  data[9] = 0x00;
-  data[10] = 0x00;
-  data[11] = 0x00;
-  data[12] = 0x00;
-  data[13] = 0x00;
-  data[14] = 0x00;
   data[15] = sum_bytes(data);
 
-  Serial.write(data, 8);
-  // Serial.println("=============" + String(pedal));
+  Serial.write(data, 16);
 }
 
 void CanPacket::send_bytes() {
   unsigned char data[16] = {0};
 
-  data[0] = CAN_PACKET_ID;
+  data[0] = packet_id;
   data[1] = uint8_t((high >> 24) & 0xFF);
   data[2] = uint8_t((high >> 16) & 0xFF);
   data[3] = uint8_t((high >> 8) & 0xFF);
